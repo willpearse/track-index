7d01cb01e80ea997ec06aa2b88c46f094f522c37
Will Pearse <will.pearse@gmail.com>
2019-08-15 15:10:27 -0600

    Cleaned index calc

--- src/headers.R
+++ src/headers.R
@@ -22,13 +22,13 @@ if(gbif.dir!="~/bear-share/gbif/")
     warning("GBIF data is not in default directory: ensure `download-gbif.rb` is similarly updated")
 
 cru.dir <- "~/bear-share/cru/"
-if(gbif.dir!="~/bear-share/cru/")
+if(cru.dir!="~/bear-share/cru/")
     warning("CRU data is not in default directory: ensure `download-cru.rb` is similarly updated")
 
 output.dir <- "clean-data/"
 
 options(mc.cores=24)
-if(options("mc.cores"==24))
+if(options("mc.cores")==24)
     warning("Assuming you have 24 cores on your computer: if not, change `headers.R`")
 
 ####################################
@@ -78,7 +78,7 @@ prog.bar <- function(x, y){
     name <- paste0("taxon_", i, "_results.RDS")
     data <- wrap.list[[i]]
     quantiles <- seq(.1,.9,.05)
-    results <- array(dim=c(length(unique(data$scientificname)),ncol=14,length(quantiles)), dimnames=list(unique(data$scientificname), c("present","present.mad","past","past.mad","projected","projected.mad","n.pre","n.post","r.pre","r.post","r.proj","r.total","pre.lat","post.lat"), quantiles))
+    results <- array(dim=c(length(unique(data$scientificname)),ncol=14,length(quantiles)), dimnames=list(unique(data$scientificname), c("index","rank.pres","rank.past"), quantiles))
     for(i in seq_len(nrow(results))){
         # Pre-allocation and calculation
         curr.data <- data[scientificname == rownames(results)[i],]
@@ -102,46 +102,34 @@ prog.bar <- function(x, y){
             pre.data[year==pre.years[k],]$post.env <- extract(temp[[pre.data$year[k]-1900+25]], pre.data[year==pre.years[k],.(decimallongitude,decimallatitude)])
         }
 
-        # Calculate differently depending on sampling
-        if(nrow(pre.data) < nrow(post.data)){
-            # More data post-1980 than before; sub-sample accordingly
-            results[i,"past",] <- quantile(pre.data$pre.env, probs=quantiles, na.rm=TRUE)
-            results[i,"projected",] <- quantile(pre.data$post.env, probs=quantiles, na.rm=TRUE)
-            results[i,"past.mad",] <- results[i,"projected.mad",] <- 0
-            t <- replicate(100, sample(post.data$env, nrow(pre.data)), simplify=FALSE)
-            results[i,"present",] <- apply(sapply(t, quantile, probs=quantiles, na.rm=TRUE), 1, median)
-            results[i,"present.mad",] <- apply(sapply(t, quantile, probs=quantiles, na.rm=TRUE), 1, mad)
-        } else {
-            if(nrow(pre.data) > nrow(post.data)){
-                # More data pre-1980 than post; sub-sample accordingly
-                t <- replicate(100, sample(pre.data$pre.env, nrow(post.data)), simplify=FALSE)
-                results[i,"past",] <- apply(sapply(t, quantile, probs=quantiles, na.rm=TRUE), 1, median)
-                results[i,"past.mad",] <- apply(sapply(t, quantile, probs=quantiles, na.rm=TRUE), 1, mad)
-                t <- replicate(100, sample(pre.data$post.env, nrow(post.data)), simplify=FALSE)
-                results[i,"projected",] <- apply(sapply(t, quantile, probs=quantiles, na.rm=TRUE), 1, median)
-                results[i,"projected.mad",] <- apply(sapply(t, quantile, probs=quantiles, na.rm=TRUE), 1, sd)
-                results[i,"present",] <- quantile(post.data$env, probs=quantiles, na.rm=TRUE)
-                results[i,"present.mad",] <- 0
-            } else {
-                # What are the odds? The same amount of data in each period
-                results[i,"past",] <- quantile(pre.data$pre.env, probs=quantiles, na.rm=TRUE)
-                results[i,"projected",] <- quantile(pre.data$post.env, probs=quantiles, na.rm=TRUE)
-                results[i,"present",] <- quantile(post.data$env, probs=quantiles, na.rm=TRUE)
-                results[i,"past.mad",] <- results[i,"projected.mad",] <- results[i,"present.mad",] <- 0
-            }
-        }
-
-        results[i,"n.pre",] <- nrow(pre.data)
-        results[i,"n.post",] <- nrow(post.data)
-
-        results[i,"r.pre",] <- diff(range(pre.data$pre.env, na.rm=TRUE))
-        results[i,"r.proj",] <- diff(range(pre.data$post.env, na.rm=TRUE))
-        results[i,"r.post",] <- diff(range(post.data$env, na.rm=TRUE))
-        results[i,"r.total",] <- diff(range(c(post.data$env,pre.data$post.env,pre.data$pre.env), na.rm=TRUE))
-
-        results[i,"pre.lat",] <- median(pre.data$decimallatitude, na.rm=TRUE)
-        results[i,"post.lat",] <- median(post.data$decimallatitude, na.rm=TRUE)
+        # Calculate for all quantiles
+        results[i,,] <- sapply(quantiles, function(x) .calc.metric(pre.data$pre.env, post.data$env, pre.data$post.env, x))
     }
     saveRDS(results, name)
     return(results)
+}
+
+
+.calc.metric <- function(present, past, projected, quantile=.5, n.boot=99){
+    # Calculate observed quantiles
+    m.present <- quantile(present, quantile)
+    m.past <- quantile(past, quantile)
+    m.projected <- quantile(projected, quantile)
+    index <- (m.present-m.past) / (m.projected-m.past)
+
+    # Bootstrap present in past
+    boots <- replicate(n.boot, sample(present, length(past)), simplify=FALSE)
+    boots <- apply(sapply(boots, quantile, probs=quantile, na.rm=TRUE), 1, function(x) (x-m.past)/(m.projected-m.past))
+    r.index.past <- rank(c(index,boots))
+
+    # Bootstrap past in present
+    boots <- replicate(n.boot, sample(past, length(present)), simplify=FALSE)
+    boots <- apply(sapply(boots, quantile, probs=quantile, na.rm=TRUE), 1, function(x) (m.present-x)/(m.projected-x))
+    r.index.pres <- rank(c(index,boots))
+     
+    # Format and return
+    return(setNames(
+        c(index, r.index.past, r.index.pres, quantile),
+        c("index","rank.pres","rank.past", quantile)
+    ))
 }
